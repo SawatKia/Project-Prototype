f48e05df538395444ab063f1e1293325d0f28768
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-01-29 21:49:56 +0700

    edit for deploying

--- Backend/CUD-student/functions/.eslintrc.js
+++ Backend/CUD-student/functions/.eslintrc.js
@@ -6,15 +6,14 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
     "google",
-    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
--- Backend/CUD-student/functions/index.js
+++ Backend/CUD-student/functions/index.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-studentApi = express();
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
+const cors = require("cors");
+const studentApi = express();
 studentApi.use(cors({
-    origin: true
+  origin: true,
 }));
 
-studentApi.get("/", (req,res) => {
-    return res.status(200).send({
-        statusCode: "success",
-        message: "You are now connedted to the Firebase"
-    })
-})
+studentApi.get("/", (req, res) => {
+  return res.status(200).send({
+    statusCode: "success",
+    message: "You are now connedted to the Firebase",
+  });
+});
