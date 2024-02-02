872080493b8c967ba07a66388a42c05efa540fc8
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-02-02 16:18:36 +0700

    delete exceed duplicate method

--- Backend/CUD-student/functions/src/controllers/stocksController.ts
+++ Backend/CUD-student/functions/src/controllers/stocksController.ts
@@ -9,7 +9,7 @@ import {ApiStockResponse} from "../interfaces/ApiStockResponse";
 // eslint-disable-next-line new-cap
 const stockController = Router();
 
-stockController.get("/get/price/:symbol/:market", async (req: Request, res: Response) => {
+stockController.get("/get/currentprice/:symbol/:market", async (req: Request, res: Response) => {
   const {symbol, market} = req.params;
   let symbolWithMarket = symbol;
   if (market === "Thai") {
@@ -20,7 +20,7 @@ stockController.get("/get/price/:symbol/:market", async (req: Request, res: Resp
     res.status(200).json({
       status: "Success",
       message: `stock ${symbol} in ${market} market gets successfully`,
-      data: currentStockPrice,
+      data: currentStockPrice.body.currentPrice.raw,
     });
   } catch (error) {
     res.status(500).json({
@@ -29,20 +29,4 @@ stockController.get("/get/price/:symbol/:market", async (req: Request, res: Resp
     });
   }
 });
-stockController.get("/get/price/:symbol", async (req: Request, res: Response) => {
-  try {
-    const currentStockPrice: ApiStockResponse = await stockservice.getStockData(req.params.symbol);
-    res.status(200).json({
-      status: "Success",
-      message: `stock ${req.params.symbol} gets successfully`,
-      data: currentStockPrice.body.currentPrice.fmt,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: "An external API error occurred",
-    });
-  }
-});
-
 export default stockController;
